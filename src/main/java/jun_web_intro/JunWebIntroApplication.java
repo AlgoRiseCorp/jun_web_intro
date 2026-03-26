5bfb6519ebcb49b13edf19bf28875861c3b63525
JunYoshizawa <j.yoshizawa@algoriseco.onmicrosoft.com>
2026-03-26 17:28:31 +0900

    Spring Boot template 作成

---
+++ src/main/java/jun_web_intro/JunWebIntroApplication.java
@@ -0,0 +1,13 @@
+package jun_web_intro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunWebIntroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunWebIntroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jun_web_intro/JunWebIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package jun_web_intro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunWebIntroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
